e78e6c80bd1e60f0b4cc310629d4316a79291645
vjenacka <edins613@gmail.com>
2019-08-10 13:06:25 +0200

    part 2 of the project

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -3,6 +3,7 @@ import './Business.css';
 
 export class Business extends Component {
   render() {
+    const {business}=this.props
     return (
       <div className="Business">
         <div className="image-container">
@@ -31,15 +32,4 @@ export class Business extends Component {
 
 export default Business;
 
-const business = {
-    imageSrc:
-      "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-    name: "MarginOtto Pizzeria",
-    address: "1010 Paddington Way",
-    city: "Flavortown",
-    state: "NY",
-    zipCode: "10101",
-    category: "Italian",
-    rating: 4.5,
-    reviewCount: 90
-  };
+
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,7 @@ export class BusinessList extends Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {this.props.businesses.map(business=><Business business={business}/>)}
       </div>
     );
   }
